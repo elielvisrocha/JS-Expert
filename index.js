75acee775b5279e9629e7f6d689367fb90ee3d05
YashiroYsana <rochaelielvis@gmail.com>
2023-02-15 19:28:29 -0300

    JAVASCRIPT SESSION

---
+++ index.js
@@ -0,0 +1,21 @@
+// Variables and values;
+
+// let continent = `America`;
+// let country = `Brazil`;
+// let population = 200;
+
+
+// console.log(
+//     `My country stay at ${continent},
+// I live on ${country},
+// and here there are ${population} million of people.
+// `);
+
+
+
+
+
+// Data Types;
+
+
+
