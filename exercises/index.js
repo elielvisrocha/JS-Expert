e3c1ce84326c88edc36355da0cea3a62c716cfcc
YashiroYsana <rochaelielvis@gmail.com>
2023-02-19 20:41:45 -0300

    JS EXPERT

--- exercises/index.js
+++ exercises/index.js
@@ -1,5 +1,54 @@
-let isIsland = false;
-let language;
+// let isIsland = false;
+// let language;
 
-console.log(typeof isIsland);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof language);
+
+
+
+
+
+
+//CHALLENGE EXERCISES
+
+
+
+const Marks = {
+    weight: 78,
+    tall: 1.69
+};
+
+const John = {
+    weight: 92,
+    tall: 1.95
+}
+
+// const Marks = {
+//     weight: 95,
+//     tall: 1.88
+// };
+
+// const John = {
+//     weight: 85,
+//     tall: 1.76
+// };
+
+
+
+let calculateBMIMark = Marks.weight / Marks.tall ** 2;
+console.log(calculateBMIMark);
+
+let calculateBMIJohn = John.weight / John.tall ** 2;
+console.log(calculateBMIJohn);
+
+let markeHigherBMI = calculateBMIJohn < calculateBMIMark;
+
+console.log(markeHigherBMI);
+
+if (calculateBMIJohn < calculateBMIMark) {
+    console.log(`Marks BMI ${calculateBMIMark.toFixed(0)} is higher than John ${calculateBMIJohn.toFixed(0)}.`);
+} else {
+    console.log(`John BMI ${calculateBMIJohn.toFixed(0)} is higher than Marks ${calculateBMIMark.toFixed(0)}.`);
+};
+
+
--- index.js
+++ index.js
@@ -18,4 +18,99 @@
 // Data Types;
 
 
+//Primitive
 
+
+//let name = "Eliélvis"; STRING
+//let age = 20; NUMBER
+//let isAlive = true; BOOLEAN
+//let x; UNDEFINED
+//let isNull = NULL;
+//let unique = Symbol("Single"); SYMBOL;
+//let bigNumber = BigInt(200); BIGINT
+
+
+//Object
+
+// let person = {
+//     name: "Weider"
+// }
+
+
+
+//BASIC OPERATORS;
+
+
+// let myBirthday = 2003;
+// let discoverAge = new Date();
+// let result = discoverAge.getFullYear() - myBirthday;
+
+// console.log(result);
+
+
+
+// let power = 2 ** 3;
+// console.log(power);
+
+// let number = 10;
+// let result = number += 29;
+// console.log(result);
+// result = number -= 20;
+// console.log(result);
+// console.log(number);
+// result = number / 2;
+// console.log(result);
+
+// let change = 19
+
+
+
+
+
+//STRING and TEMPLATE LITERALS
+
+
+// const firsName = "Eliélvis";
+// const job = "Developer";
+// const birthYear = 2003;
+
+// const describeMe = "I'm " + firsName + " my current job is as " + job + " and my birth year is " + birthYear;
+
+// console.log(describeMe);
+
+
+// const describeMeAgain = `I'm ${firsName} my current job is as ${job} and my birth year is ${birthYear}`;
+
+
+// console.log(describeMeAgain);
+
+// console.log(`multi
+// line
+// strings`)
+
+// console.log("multi \n\
+// line \n\
+// strings")
+
+const age = 19;
+const checkAge = age >= 18;
+
+if (checkAge) {
+    console.log(`Can join in the party`);
+}
+else {
+    console.log(`Can't join in the party`);
+};
+
+
+
+
+const birthYear = 2003;
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+    console.log(century)
+} else {
+    century = 21;
+    console.log(century)
+}; 
