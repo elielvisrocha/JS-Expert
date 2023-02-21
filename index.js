92d1a21e5803f06ffe2292c6c1b83a84158080d9
YashiroYsana <rochaelielvis@gmail.com>
2023-02-20 22:26:07 -0300

    more JS

--- index.js
+++ index.js
@@ -4,22 +4,15 @@
 // let country = `Brazil`;
 // let population = 200;
 
-
 // console.log(
 //     `My country stay at ${continent},
 // I live on ${country},
 // and here there are ${population} million of people.
 // `);
 
-
-
-
-
 // Data Types;
 
-
 //Primitive
-
 
 //let name = "Eliélvis"; STRING
 //let age = 20; NUMBER
@@ -29,25 +22,19 @@
 //let unique = Symbol("Single"); SYMBOL;
 //let bigNumber = BigInt(200); BIGINT
 
-
 //Object
 
 // let person = {
 //     name: "Weider"
 // }
 
-
-
 //BASIC OPERATORS;
-
 
 // let myBirthday = 2003;
 // let discoverAge = new Date();
 // let result = discoverAge.getFullYear() - myBirthday;
 
 // console.log(result);
-
-
 
 // let power = 2 ** 3;
 // console.log(power);
@@ -63,12 +50,7 @@
 
 // let change = 19
 
-
-
-
-
 //STRING and TEMPLATE LITERALS
-
 
 // const firsName = "Eliélvis";
 // const job = "Developer";
@@ -78,9 +60,7 @@
 
 // console.log(describeMe);
 
-
 // const describeMeAgain = `I'm ${firsName} my current job is as ${job} and my birth year is ${birthYear}`;
-
 
 // console.log(describeMeAgain);
 
@@ -102,9 +82,6 @@
 //     console.log(`Can't join in the party`);
 // };
 
-
-
-
 // const birthYear = 2003;
 // let century;
 // if (birthYear <= 2000) {
@@ -115,19 +92,7 @@
 //     console.log(century)
 // };
 
-
-
-
-
-
-
-
-
-
-
-
 //TYPE CONVERSION
-
 
 // let number = "2000";
 // console.log(typeof number);
@@ -136,17 +101,139 @@
 // let x = Number(number);
 // console.log(typeof x);
 
-
 //TYPE COERSION
- 
-console.log("I'm " + 19 + " years old");
-console.log(90 - "20" - 10);
-console.log(90 * "20" * 10);
+
+// console.log("I'm " + 19 + " years old");
+// console.log(90 - "20" - 10);
+// console.log(90 * "20" * 10);
+
+//BOOLEAN VALUES
+//THERE ARE 5 FALSY VALURES: 0, "", UNDEFINED, NULL, NAN
+
+// console.log(Boolean(0))
+// console.log(Boolean(undefined))
+// console.log(Boolean(""))
+// console.log(Boolean(null))
+// console.log(Boolean(NaN))
+
+// const money = 0;
+
+// if(money) {
+//     console.log(`Don't spend it all ;`);
+// } else {
+//     console.log(`You should get a job!`);
+// }
+
+// let height;
+
+// if(height) {
+//     console.log(`OMG YOU'RE SO TALL`);
+// } else {
+//     console.log(`Pls, tell me how tall you're!`);
+// }
+
+//EQUALITY OPERATORS
+
+// const age = '18';
+
+// if(age == 18) console.log(`You became an adult!!(loose)`)
+// if(age === 18) console.log(`You became an adult!!(strict)`)
+
+// if(age === 18) {
+//     console.log(`You can drive!!!`);
+// } else {
+//     console.log(`You can't drive anyway!!!`);
+// }
+
+// const checkDayOfWeek = prompt(`What's your favorite day of week?`);
+
+// const days = [
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+//   "Sunday",
+// ];
+
+// const discoverDay = days.find((value) => {
+//   if (checkDayOfWeek === value) {
+//     alert(`You said ${value}`);
+//   } else if (
+//     checkDayOfWeek === "" ||
+//     checkDayOfWeek === NaN ||
+//     checkDayOfWeek === 0 ||
+//     checkDayOfWeek === undefined
+//   ) {
+//     alert(`Please tell us a day of week you more like!!!`);
+//   } else if (checkDayOfWeek != value) {
+//     alert(`Oh no, tell us!`);
+//   }
+// });
+
+//BOOLEAN LOGIC
+
+// const hasDriversLicense = true; //A
+// const hasGoodVision = false; //B
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+// console.log(!hasGoodVision);
+
+// const shouldDriveToTravel = hasDriversLicense || hasGoodVision;
+// const shouldDriveToTravel = hasDriversLicense && hasGoodVision;
+// const shouldDriveToTravel = !hasDriversLicense;
+// const shouldDriveToTravel = !hasGoodVision;
+
+// if(shouldDriveToTravel) {
+//     console.log(`You can drive`);
+// } else {
+//     console.log(`We don't recommend you drive!`);
+// }
+
+//SWITCH STATAMENT
+
+const day = 2;
+
+if (day === 1) {
+    console.log("Monday");
+} else if (day === 2) {
+    console.log("Tuesday");
+} else if (day === 3) {
+    console.log("Wednesday");
+} else if (day === 4) {
+    console.log("Thursday");
+} else if (day === 5) {
+    console.log("Friday");
+} else if (day === 6) {
+    console.log("Saturday");
+} else if (day === 7) {
+    console.log("Sunday");
+} 
 
 
-
-
-
-
-
-''
+switch (day) {
+  case 1:
+    console.log(`Monday`);
+    break;
+  case 2:
+    console.log(`Tuesday`);
+    break;
+  case 3:
+    console.log(`Wednesday`);
+    break;
+  case 4:
+    console.log(`Thursday`);
+    break;
+  case 5:
+    console.log(`Friday`);
+    break;
+  case 6:
+    console.log(`Saturday`);
+    break;
+  case 7:
+    console.log(`Sunday`);
+    break;
+}
